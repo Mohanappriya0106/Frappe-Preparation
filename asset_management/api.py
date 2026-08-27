38b63f9fa6b54efac310a5f8948563e7ee58b3c1
Mohanappriya0106 <mohanappriyakarunakaran@gmail.com>
2026-08-27 11:41:58 +0530

    utility functions

--- asset_management/api.py
+++ asset_management/api.py
@@ -5,7 +5,7 @@ def api_practice():
     customer=frappe.qb.DocType("Customer")
     address=frappe.qb.DocType("Customer Address")
 
-    query=frappe.qb.from_(customer).join(address).on(customer.name == address.customer_id).where(customer.status== "InActive").select(customer.customer_name)
+    query=frappe.qb.from_(customer).join(address).on(customer.name == address.customer_id).where(customer.status== "InActive").select(customer.name,customer.customer_name)
 
     result=query.run(as_dict = True)
 
@@ -13,3 +13,30 @@ def api_practice():
         frappe.db.set_value("Customer", row.name, "status", "Active")
 
     return result
+
+
+
+
+@frappe.whitelist()
+def get_recent_todos():
+
+    customers = frappe.get_list(
+        "To Do",
+        order_by="creation desc",
+        limit_page_length=5
+    )
+
+    for customer  in customers:
+        customer["owner_email"] = frappe.db.get_value(
+            "Customer",
+            customers.name,
+            "email"
+        )
+
+
+    timestamp = frappe.utils.now()
+
+    return {
+        "timestamp": timestamp,
+        "records": customers
+    }
---
+++ asset_management/asset_management/doctype/to_do/to_do.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Employees report issues. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("To Do", {
+// 	refresh(frm) {
+
+// 	},
+// });
